919205217da22d8c609285162b2aa7f7b4ca628f
devilvis2024 <54133947+devilvis@users.noreply.github.com>
2019-10-04 18:16:43 +0530

    Program that implements Mid-point circle Algo.

---
+++ MCA.c
@@ -0,0 +1,45 @@
+
+/*Midpoint Circle Algorithm Example Program In C */
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+
+void main() {
+   int gd = DETECT, gm;
+   int x, y, r;
+   void Drawcircle(int x, int y, int r);
+   printf("Enter the Mid points and Radius:",&&&&);
+   scanf("%d%d%d", &x, &y, &r);
+   initgraph(&gd, &gm, "C:/TURBOC3/BGI");
+   Drawcircle(x, y, r);
+   getch();
+   closegraph();
+}
+
+void Drawcircle(int x1, int y1, int r) {
+   int x = 0, y = r, p = 1 - r;
+   void cliplot(int x1, int y1, int x, int y);
+   cliplot(x1, y1, x, y);
+   while (x < y) {
+      x++;
+      if (p < 0)
+     p += 2 * x + 1;
+      else {
+     y--;
+     p += 2 * (x - y) + 1;
+      }
+      cliplot(x1, y1, x, y);
+   }
+}
+
+void cliplot(int x0, int y0, int x, int y) {
+   putpixel(xctr + x, y0 + y, 1);
+   putpixel(xctr - x, y0 + y, 1);
+   putpixel(xctr + x, y0 - y, 1);
+   putpixel(xctr - x, y0 - y, 1);
+   putpixel(xctr + y, yctr + x, 1);
+   putpixel(xctr - y, yctr + x, 1);
+   putpixel(xctr + y, yctr - x, 1);
+   putpixel(xctr - y, yctr - x, 1);
+   getch();
+}
